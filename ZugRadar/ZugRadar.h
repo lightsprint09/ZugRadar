bb3aba920f1f61a3fcdc040829a2af0e6e25438c
Lukas Schmidt <lukas.la.schmidt@deutschebahn.com>
2017-09-22 14:32:49 +0200

    Initial Commit

---
+++ ZugRadar/ZugRadar.h
@@ -0,0 +1,19 @@
+//
+//  ZugRadar.h
+//  ZugRadar
+//
+//  Created by Lukas Schmidt on 22.09.17.
+//  Copyright © 2017 DBSystel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZugRadar.
+FOUNDATION_EXPORT double ZugRadarVersionNumber;
+
+//! Project version string for ZugRadar.
+FOUNDATION_EXPORT const unsigned char ZugRadarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZugRadar/PublicHeader.h>
+
+
